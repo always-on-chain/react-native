175e3b167c2193c50a879894bc3cffe5a4d99aef
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-10-29 21:02:11 -0700

    created first animation with ball

--- src/Ball.js
+++ src/Ball.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
+
+// Three main questions for animation:
+// 1. Where is the item right now?
+// Use Animated.ValueXY
+// 2. Where is the element?
+// Use Animated.Spring
+// 3. Which element are we moving?
+// Use Animated.View
+
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: {x: 200, y: 500}
+    }).start();
+  }
   render() {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     )
   }
 }
