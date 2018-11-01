026a8f53e4c7347dc8894e998df40dd67c8f391d
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-10-31 19:38:57 -0700

    installed react native elements lib and use components to render cards

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck.js';
+
 
 const DATA = [
   { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
@@ -14,10 +16,31 @@ const DATA = [
 ];
 
 export default class App extends React.Component {
+  renderCard(item) {
+    return (
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{uri: item.uri}}
+      >
+        <Text style={{marginBottom: 10}}>
+          I can customize the card further
+        </Text>
+        <Button
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <Deck 
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
