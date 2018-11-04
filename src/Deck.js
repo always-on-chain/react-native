34993f31704c05bd238f1ced79973ab8ea235f9e
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-11-04 13:22:45 -0800

    animated card completely out of view when swipe threshold is met on both sides

--- src/Deck.js
+++ src/Deck.js
@@ -8,6 +8,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -20,15 +21,24 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right!');
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left!');
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
       },
     });
     this.state = { panResponder, position };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH + 30 : -SCREEN_WIDTH -30;
+
+    Animated.timing(this.state.position, {
+      toValue: {x: x, y: 0},
+      duration: SWIPE_OUT_DURATION
+    }).start();
   }
 
   resetPosition() {
